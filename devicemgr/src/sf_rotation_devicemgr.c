d064c9c838c7c07dedc587541ae2a2b9412fbed4
Mun, Gwan-gyeong <kk.moon@samsung.com>
2013-04-29 14:05:13 +0900

    Change Flora License Version : 1.0 -> 1.1
    Change-Id: I624cbc68aee93adb432696fba0fe5706aa0dbe6a

--- devicemgr/src/sf_rotation_devicemgr.c
+++ devicemgr/src/sf_rotation_devicemgr.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2013 Samsung Electronics Co., Ltd. All rights reserved.
  *
  * This file is a modified version of BSD licensed file and
- * licensed under the Flora License, Version 1.0 (the License);
+ * licensed under the Flora License, Version 1.1 (the License);
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
